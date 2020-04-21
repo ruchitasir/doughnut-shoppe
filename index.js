fb56e3a1d4309b04e603ae2b4cf2c64f692ff430
brandiw <brandimichelle.0911@gmail.com>
2020-04-21 09:46:09 -0700

    Adding customer controller

--- index.js
+++ index.js
@@ -8,22 +8,9 @@ app.set('view engine', 'ejs')
 app.use(layouts)
 app.use(express.urlencoded({ extended: false }))
 
+app.use('/customers', require('./controllers/customers'))
 app.use('/donuts', require('./controllers/donuts'))
 app.use('/shops', require('./controllers/shops'))
-
-app.get('/customers/:id', (req, res) => {
-  db.customer.findOne({
-    where: { id: req.params.id },
-    include: [ db.donut ]
-  })
-  .then(customer => {
-    res.render('customers/show', { customer })
-  })
-  .catch(err => {
-    console.log(err)
-    res.send('Error')
-  })
-})
 
 app.get('/', (req, res) => {
   res.render('home')
